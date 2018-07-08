1dfe4de24209c702b0928383a8b1ca24650c53ac
daniel <daniel@iphonevschwindt>
2018-07-08 14:29:58 +0200

    initial

---
+++ src/main/java/my/vaadin/watchManager/App.java
@@ -0,0 +1,13 @@
+package my.vaadin.watchManager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
